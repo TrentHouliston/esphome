6d4f3094ab7ae910539d22192f9fdc2ba4d44803
Trent Houliston <trent@houliston.me>
2023-03-03 14:07:36 +1100

    Limit the loop

--- esphome/components/abb_aurora/abb_aurora.cpp
+++ esphome/components/abb_aurora/abb_aurora.cpp
@@ -70,12 +70,13 @@ void ABBAurora::loop() {
     this->last_request_time_ = now;
     ESP_LOGVV(TAG, "Sent request packet to inverter");
   } else {
-    while (this->available()) {
+    // while (this->available()) {
+    for (int i = 0; this->available() && i < sizeof(Response); ++i) {
       // Read into the buffer until we reach 10 bytes (all return packets are 6 bytes + crc)
       this->read_byte(this->buffer_ + this->idx_);
       this->idx_ += 1;
 
-      if (this->idx_ == 8) {
+      if (this->idx_ == sizeof(Response)) {
         // We have a full packet, process it
         this->idx_ = 0;
         this->idle_ = true;
