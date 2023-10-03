cebb97232dd198dfecb377b66bb80ba94977bc78
Olivier Berghmans <29234023+OBerghmans@users.noreply.github.com>
2023-10-03 10:51:12 +0200

    Remove duplicate definition by changing header file to declarations

--- esphome/components/pulse_meter/pulse_meter_sensor.h
+++ esphome/components/pulse_meter/pulse_meter_sensor.h
@@ -15,12 +15,12 @@ class PulseMeterSensor : public sensor::Sensor, public Component {
     FILTER_PULSE,
   };
 
-  void set_pin(InternalGPIOPin *pin) { this->pin_ = pin; }
-  void set_filter_us(uint32_t filter) { this->filter_us_ = filter; }
-  void set_timeout_us(uint32_t timeout) { this->timeout_us_ = timeout; }
-  void set_total_sensor(sensor::Sensor *sensor) { this->total_sensor_ = sensor; }
-  void set_filter_mode(InternalFilterMode mode) { this->filter_mode_ = mode; }
-  void set_total_pulses(uint32_t pulses) { this->total_pulses_ = pulses; }
+  void set_pin(InternalGPIOPin *pin);
+  void set_filter_us(uint32_t filter);
+  void set_timeout_us(uint32_t timeout);
+  void set_total_sensor(sensor::Sensor *sensor);
+  void set_filter_mode(InternalFilterMode mode);
+  void set_total_pulses(uint32_t pulses);
 
   void setup() override;
   void loop() override;
